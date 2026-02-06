e795d162cc87bdf4dc3098c6860f838ed2d67425
Moon Rathi <147794854+MRathi2303@users.noreply.github.com>
2026-02-06 23:34:25 +0530

    Time: 11 ms (35.52%) | Memory: 20.7 MB (21.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,9 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        str1 = re.sub(r'[^a-zA-Z0-9]', '', s)
-        str1 = str1.lower()
-        return str1 == str1[::-1]
+        s1 = re.sub(r'[^a-zA-Z0-9]', '', s)
+        s1 = s1.lower()
 
+        start = 0
+        end = len(s1) - 1
 
-# just converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters nad comparing the strings
-        
+        while start < end:
+            if s1[start] != s1[end]:
+                return False
+            start += 1
+            end -= 1
+
+        return True
+
+## using two pointer and iterating till the elements are same from start and end 
